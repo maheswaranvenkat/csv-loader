eae394819d3ce9098c0dcb126c0d8be0399de8b0
Mahesh <maheswaran.venkatraman@kbc.be>
2018-06-18 15:18:25 +0200

    first commit

---
+++ angular-csv-parser/src/app/pipes/sorting-user.pipe.spec.ts.js
@@ -0,0 +1,8 @@
+import { SortingCompaniesPipe } from './sorting-users.pipe';
+
+describe('SortingCompaniesPipe', () => {
+  it('create an instance', () => {
+  const pipe = new SortingCompaniesPipe();
+  expect(pipe).toBeTruthy();
+});
+});
